9604774f54eefd96a6e53a1263cbc2684c037201
muhammadzhuhry <muhammadzhuhry@gmail.com>
2019-08-27 00:09:20 +0700

    add helper send email

---
+++ helper/sendEmail.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const mustache = require('mustache');
+const nodemailer = require('nodemailer');
+
+module.exports = {
+  async sendMailRegister(payload) {
+    const template = fs.readFileSync('./helper/template.html', 'utf8');
+
+    const config = {
+      service: 'gmail',
+      auth: {
+        user: 'youremail@gmail.com',
+        pass: 'yourpassword'
+      }
+    };
+    const transporter = await nodemailer.createTransport(config);
+    const mail = {
+      to: payload.email,
+      from: 'node-email.test@gmail.com <no-reply@node-email.com>',
+      subject: 'Node Email Registration',
+      html: mustache.to_html(template, { ...payload }),
+    };
+    transporter.sendMail(mail);
+  }
+};
